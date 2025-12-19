1c02a0f9e25fe661a70a0f2a6bcfb67846211ffb
JD10S <dodevp10@gmail.com>
2025-12-19 10:30:23 -0500

    Trigger redeploy

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ const API_BASE_URL = import.meta.env.VITE_API_URL ;
 
 export async function login(data) {
   try {
-    const response = await fetch(`${API_BASE_URL}/users/login`, {
+    const response = await fetch(`${API_BASE_URL}/api/users/login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
